de28afe96890f9606817479a3e1aad283d9cd972
Avinash010203 <90713835+Avinash010203@users.noreply.github.com>
2023-02-09 08:19:32 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,39 @@
+class addition1{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)  
+ {    
+    BigDecimal x = new BigDecimal(input1);
+        BigDecimal y = new BigDecimal(input2);
+        return String.valueOf(x.add(y));
+ }    
+  
+}} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BigDecimal x = new BigDecimal(input1);
+        BigDecimal y = new BigDecimal(input2);
+        return String.valueOf(x.add(y));
